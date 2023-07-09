b771e84aa8d7966e1dba5628c7fbab4c922b9df4
Matthew Lyon <matthew@lyonheart.us>
2023-07-09 15:22:15 -0700

    unfix there-and-back

--- 1/a/there_and_back.test.ts
+++ 1/a/there_and_back.test.ts
@@ -7,10 +7,7 @@ test("can re-hydrate serialized values", () => {
   fc.assert(
     fc.property(
       // generate a date, then convert it to DateTime
-      fc.date({
-        min: new Date(-62167190880000),
-        max: new Date(253402329599999)
-      }).map(d => DateTime.fromJSDate(d)),
+      fc.date().map(d => DateTime.fromJSDate(d)),
       // a predicate to validate
       (dt) => {
         expect(dt).toEqual(DateTime.fromSQL(dt.toSQL()))
